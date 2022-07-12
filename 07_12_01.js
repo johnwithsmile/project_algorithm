7e3f7710f9eccb1764d58966de86d4f6289f1e47
Jeongdong Kim <93374751+johnwithsmile@users.noreply.github.com>
2022-07-12 18:28:06 +0900

    let&const

---
+++ 07_12_01.js
@@ -0,0 +1,16 @@
+/* 
+
+기초부터 재밌게 다시 해보는 알고리즘도 진행
+
+'name'이라는 변수를 선언하고, 자신의 이름을 할당하세요.
+
+주의사항
+const, let, var사용
+
+예상결과
+console.log(name) // '영희'
+
+*/
+
+const name = "영희";
+console.log(name);
---
+++ 07_12_02.js
@@ -0,0 +1,24 @@
+/* 
+
+2. 재할당
+주어진 변수 'name'에 '철수'를 재할당하세요.
+
+주의사항
+const, let, var 중 재할당 할 수 없는 것은 무엇일까요. 
+
+예상결과
+console.log(name) // '철수'
+
+*/
+
+const name = "철수";
+console.log(name);
+/* 
+ 
+var: 재선언 o, 재할당 o
+let: 재선언 x, 재할당 o
+const: 재선언 x, 재할당 x
+
+
+
+ */

f04b01b585d5224c8b22e5cad0cafc2a7968b252
Jeongdong Kim <93374751+johnwithsmile@users.noreply.github.com>
2022-07-14 18:21:09 +0900

    control array

---
+++ 07_14_01.js
@@ -0,0 +1,22 @@
+/* 
+
+4 배열의 기능
+
+fruits = ["사과", "바나나", "파인애플"];
+주어진 fruits 배열에서 마지막 요소를 꺼내 newfruits에 넣어주세요.
+
+주의사항 length를 이용해서 문제를 풀어야 합니다
+push를 이용해서 문제를 풀어야 합니다.
+마지막 요소를 꺼낼 때 length를 사용하는 이유는?
+*/
+
+const fruits = ["사과", "바나나", "파인애플"];
+const lastfruits = fruits[fruits.length - 1];
+const newfruits = [];
+newfruits.push(lastFruits);
+
+/* 
+ 
+ length는 계속 바뀔 수 있기 때문에 length를 써야함
+
+ */

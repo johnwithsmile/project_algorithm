86dbf2710f680fdfbc6fe8f72d2548a0acd5eb51
Jeongdong Kim <93374751+johnwithsmile@users.noreply.github.com>
2022-07-28 13:38:23 +0900

    biggest sort update

--- 07_28_01.js
+++ 07_28_01.js
@@ -1,37 +1,31 @@
 /* 
 
-26. 홀수 문자열
-
-num을 입력받아 1부터 num까지의 숫자 중 홀수로 구성된 문자열을 만들어야 합니다.
-
-num이 5일 경우에는 "135"입니다.
-입력 인자
-num은 숫자열입니다.
-
-주의사항
-for을 이용해서 문제를 풀어야 합니다.
-짝를 제외하는 조건을 추가해야합니다.
-
-예상 결과 
-makeOdd(5) // "135"
-makeOdd(7) // "1357"
+27. 가장 큰 수 가져오기 
 
 */
 
-function makeOdd(num) {
-  let str = "";
-  for (let i = 1; i <= num; i++) {
-    if (i % 2 === 1) {
-      str = str + i;
+function bigNum(str) {
+  let biggest = 0;
+
+  for (let i = 0; i < str.length; i = i + 1) {
+    if (Number(str[i]) > biggest) {
+      biggest = Number(str[i]);
     }
-    console.log(str);
   }
+
+  return String(biggest);
 }
 
 /* 
 
-for문으로 만들면 i가 num넣은 숫자대로 12345 하나씩 출력함
-그럼 홀수를 분류함, 2로 나눈값이 1인 숫자
+str = '12345'라고하면 제일 큰 수를 가져와야함
+let biggest = 0 이라고 설정, length까지 반복, 1씩증가
+for(let i = 0; i < str.length; i = i+){
+	console.log(str[i])
+}
+큰수 가져오기 큰수는 저장, 아니면 넘어가기
 
-그 값을 빈 str에 더하면 됨 
+typeof는 다음에 오는 것의 속성 평가, str이므로 숫자로 변경해줘야함
+
+
  */

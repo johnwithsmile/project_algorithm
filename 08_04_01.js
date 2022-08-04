20d336f16c1a0ffddf03c7aa03301ffbb8d009e1
Jeongdong Kim <93374751+johnwithsmile@users.noreply.github.com>
2022-08-04 11:27:44 +0900

    strToInt

--- 08_04_01.js
+++ 08_04_01.js
@@ -15,5 +15,12 @@ function solution(s) {
 
   문자열을 숫자로 바꾸기
   Number를 쓰면 숫자로 바뀐다.
+
+  베스트 풀이를 보면 이렇게도 푼다. 1로 나뉘면 숫자로 바뀌어버리는것
+  조금 더 생각했으면 생각났을텐데
+  function strToInt(str){
+  return str/1
+  }
+
   
  */

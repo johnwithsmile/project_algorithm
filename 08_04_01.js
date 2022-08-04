48b266f384800a48bd72a97cc0498d59cdb6dcee
Jeongdong Kim <93374751+johnwithsmile@users.noreply.github.com>
2022-08-04 10:18:46 +0900

    strToInt

---
+++ 08_04_01.js
@@ -0,0 +1,19 @@
+/* 
+
+문자열을 정수로 바꾸기
+https://school.programmers.co.kr/learn/courses/30/lessons/12925
+
+*/
+
+function solution(s) {
+  let answer = 0;
+  answer = Number(s);
+  return answer;
+}
+
+/* 
+
+  문자열을 숫자로 바꾸기
+  Number를 쓰면 숫자로 바뀐다.
+  
+ */
